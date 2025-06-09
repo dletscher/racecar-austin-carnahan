232cc13c4f10eff8a22bc8a653fb51ecd53c2e0a
austin c <ac6404@gmail.com>
2025-06-08 22:11:30 -0500

    revert to better, but slower driver

--- MyAgent.py
+++ MyAgent.py
@@ -9,23 +9,14 @@ class Agent:
 		ahead = observations['lidar'][2]
 		right_front = observations['lidar'][3]
 		right = observations['lidar'][4]
-
-		on_straightaway = ahead > 1.8 and left_front > 1.5 and right_front > 1.5
 	
-		max_speed = 0.30 if on_straightaway else 0.22
 
-		# Speed constraints, max/min
+		# Implement your rule-based decision-making here
 		if speed < 0.05:
 			return ('straight', 'accelerate')
-		if speed > max_speed:
+		if speed > 0.22:
 			return ('straight', 'brake')
-		
-		# Detect turns and brake or coast
-		if ahead < 1.5 and speed > 0.23:
-			if left_front > right_front:
-				return ('left', 'brake')
-			else:
-				return ('right', 'brake')
+
 		if ahead < 1:
 			if left_front > right_front:
 				return ('left', 'brake')
@@ -36,8 +27,6 @@ class Agent:
 				return ('left', 'coast')
 			else:
 				return ('right', 'coast')
-			
-		# Keep away from the walls
 		else:
 			if left < 1.2:
 				return ('right', 'accelerate')
