290676e5c9b6439053bf665967407a4a448dc0b1
austin c <ac6404@gmail.com>
2025-06-08 23:48:06 -0500

    add back in straightaway detection

--- MyAgent.py
+++ MyAgent.py
@@ -10,19 +10,25 @@ class Agent:
 		right_front = observations['lidar'][3]
 		right = observations['lidar'][4]
 	
+		on_straightaway = ahead > 3 and left_front > 1.8 and right_front > 1.8
+
+		# if on_straightaway:
+		# 	print("On straightaway")
+
+		max_speed = 0.38 if on_straightaway else 0.26
 
 		# Implement your rule-based decision-making here
-		if speed < 0.05:
+		if speed < 0.1:
 			return ('straight', 'accelerate')
-		if speed > 0.22:
+		if speed > max_speed:
 			return ('straight', 'brake')
 
-		if ahead < 1:
+		if ahead < 1.5:
 			if left_front > right_front:
 				return ('left', 'brake')
 			else:
 				return ('right', 'brake')
-		if ahead < 0.5:
+		if ahead > 1.5 and ahead < 3.0:
 			if left_front > right_front:
 				return ('left', 'coast')
 			else:
--- development.md
+++ development.md
@@ -9,3 +9,18 @@
 
 3. Add straightaway detection to bump max speed. Start slowing and turning earlier at high speeds. Avg. Score - 129
 
+4. Revert to original setup. Better at following the line, but slower. Fix ahead distance check so coasting actually works. Score 126
+
+5. try to re-implement straightaway detection. score: 123.5 ahead threshhold 0.5 max speed 4
+
+6. improve straightaway thresholds. score 129. ahead 4, max speed 32, add frontleft and frontright at 2
+
+7. set leftfront and rightfront straightwaway thresholds to 1.8 -- score 130
+
+8. increase max speed to 36 score 132
+
+9. bump speed to 4 and coast threshold from 2.5 to 3 --- score 128
+
+10. max speed 38, reg speed bump to 24. ahead 3, lf, rf 1.8, coast threshold 3 --- 133
+
+
